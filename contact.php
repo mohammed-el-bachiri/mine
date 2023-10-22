ac65eb6442eef57f4032b8d19b583d206ad863a4
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-10-22 20:28:09 +0100

    add style

--- contact.php
+++ contact.php
@@ -3,17 +3,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $full_name = $_POST["full_name"];
     $phone = $_POST["phone"];
     $email = $_POST["email"];
-    $subject = $_POST["subject"];
     $message = $_POST["message"];
 
-    $data = "Full Name: $full_name\nPhone: $phone\nEmail: $email\nSubject: $subject\nMessage:\n$message\n\n". "________________________________________________________\n\n";
+    $data = "Full Name: $full_name\nPhone: $phone\nEmail: $email\nMessage:\n$message\n\n". "________________________________________________________\n\n";
 
     $file = 'contacts.txt';
 
     file_put_contents($file, $data, FILE_APPEND);
+    echo "thank you";
 
-    sleep(3);
+    sleep(2);
     header("Location: index.html");
-    exit;
+
 }
 ?>
